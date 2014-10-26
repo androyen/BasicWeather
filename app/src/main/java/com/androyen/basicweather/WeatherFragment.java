188d6e4df33296435124c167c1b05fbe3447828f
Androyen <androyen@aol.com>
2014-10-26 10:50:42 -0500

    Used setWeatherIcon method

--- app/src/main/java/com/androyen/basicweather/WeatherFragment.java
+++ app/src/main/java/com/androyen/basicweather/WeatherFragment.java
@@ -10,6 +10,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 /**
@@ -72,6 +79,10 @@ public class WeatherFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        //Set up the weatherFont icon
+        weatherFont = Typeface.createFromAsset(getActivity().getAssets(), "fonts/weather.ttf");
+        updateWeatherData(new CityPreference(getActivity()).getCity());
     }
 
     @Override
@@ -88,6 +99,103 @@ public class WeatherFragment extends Fragment {
         return rootView;
 
 
+    }
+
+    //Get weather data
+    private void updateWeatherData(final String city) {
+
+        //Create new asynchronous thread
+        new Thread() {
+
+            public void run() {
+                final JSONObject json = RemoteFetch.getJSON(getActivity(), city);
+
+                if (json == null) {
+                    handler.post(new Runnable() {
+
+                        public void run() {
+                            Toast.makeText(getActivity(), getActivity().getString(R.string.place_not_found), Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+                else {
+                    handler.post(new Runnable() {
+
+                        public void run() {
+                            renderWeather(json);
+                        }
+                    });
+
+                }
+            }
+        }.start();
+    }
+
+    private void renderWeather(JSONObject json) {
+        try {
+            cityField.setText(json.getString("name").toUpperCase(Locale.US) + ", " + json.getJSONObject("sys").getString("country"));
+
+            JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = json.getJSONObject("main");
+            detailsField.setText(
+                    details.getString("description").toUpperCase(Locale.US) + "\n" + "Humidity: " + main.getString("humidity") + "%"
+                    + "\n" + "Pressure: " + main.getString("pressure") + " hPa"
+            );
+
+            currentTemperatureField.setText(
+                    String.format("%.2f", main.getDouble("temp")) + " ("
+            );
+
+            DateFormat df = DateFormat.getDateTimeInstance();
+            String updatedOn = df.format(new Date(json.getLong("dt") * 1000));
+            updatedField.setText("Last update: " + updatedOn);
+
+            setWeatherIcon(details.getInt("id"), json.getJSONObject("sys").getLong("sunrise") * 1000,
+                    json.getJSONObject("sys").getLong("sunset") * 1000);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void setWeatherIcon(int actualId, long sunrise, long sunset) {
+
+        int id = actualId / 100;
+        String icon = "";
+
+        if(actualId == 800) {
+            long currentTime = new Date().getTime();
+            if(currentTime >= sunrise && currentTime < sunset) {
+                icon = getActivity().getString(R.string.weather_sunny);
+            }
+            else {
+                icon = getActivity().getString(R.string.weather_clear_night);
+            }
+        }
+        else {
+
+            switch(id) {
+                case 2:
+                    icon = getActivity().getString(R.string.weather_thunder);
+                    break;
+                case 3:
+                    icon = getActivity().getString(R.string.weather_drizzle);
+                    break;
+                case 7:
+                    icon = getActivity().getString(R.string.weather_foggy);
+                    break;
+                case 8:
+                    icon = getActivity().getString(R.string.weather_cloudy);
+                    break;
+                case 6:
+                    icon = getActivity().getString(R.string.weather_snowy);
+                    break;
+                case 5:
+                    icon = getActivity().getString(R.string.weather_rainy);
+                    break;
+            }
+        }
+        weatherIcon.setText(icon);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
