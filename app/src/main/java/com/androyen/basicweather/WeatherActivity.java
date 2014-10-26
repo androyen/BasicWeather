bbacb46ac33c37c58401846c5861f03c3d5c8e86
Androyen <androyen@aol.com>
2014-10-26 13:18:05 -0500

    Create object in WeatherActivity

--- app/src/main/java/com/androyen/basicweather/WeatherActivity.java
+++ app/src/main/java/com/androyen/basicweather/WeatherActivity.java
@@ -1,9 +1,13 @@
 package com.androyen.basicweather;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 
 
 public class WeatherActivity extends ActionBarActivity {
@@ -33,9 +37,32 @@ public class WeatherActivity extends ActionBarActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+        if (id == R.id.change_city) {
+            showInputDialog();
         }
-        return super.onOptionsItemSelected(item);
+        return false;
+    }
+
+    private void showInputDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(R.string.dialog_change_city);
+        final EditText input = new EditText(this);
+        input.setInputType(InputType.TYPE_CLASS_TEXT);
+        builder.setView(input);
+        builder.setPositiveButton(R.string.dialog_go, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                changeCity(input.getText().toString());
+            }
+        });
+        builder.show();
+    }
+
+    private void changeCity(String city) {
+        WeatherFragment wf = (WeatherFragment)getSupportFragmentManager().findFragmentById(R.id.container);
+        wf.changeCity(city);
+
+        new CityPreference(this).setCity(city);
     }
 }
